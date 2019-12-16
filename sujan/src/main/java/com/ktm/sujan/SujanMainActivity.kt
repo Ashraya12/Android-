55cb4c069c930fb5f08f5a1ef745a50fb01b5d70
sujan999 <imsuzzank10@gmail.com>
2019-12-16 16:47:11 +0545

    login in form added

--- sujan/src/main/java/com/ktm/sujan/SujanMainActivity.kt
+++ sujan/src/main/java/com/ktm/sujan/SujanMainActivity.kt
@@ -1,12 +1,27 @@
 package com.ktm.sujan
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sujan_main.*
 
 class SujanMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sujan_main)
+
+        val name = Name.text
+        val Password = password.text
+
+
+        login.setOnClickListener() {
+
+            Toast.makeText(this@SujanMainActivity, "Logged in as $name", Toast.LENGTH_LONG).show()
+            Toast.makeText(this@SujanMainActivity, "Logged in as $Password", Toast.LENGTH_LONG).show()
+        }
     }
 }
+
