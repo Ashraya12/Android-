84186a78f131952c55d7077320616ca893c6e0ef
MrMoriarty0011 <nishant2054@gmail.com>
2019-12-19 17:00:24 +0545

    updated>>>>

--- aaryan/src/main/java/com/ktm/aaryan/ListView.kt
+++ aaryan/src/main/java/com/ktm/aaryan/ListView.kt
@@ -1,9 +1,12 @@
 package com.ktm.aaryan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.SearchView
 import kotlinx.android.synthetic.main.activity_list_view.*
+import android.widget.SearchView.OnSuggestionListener as OnSuggestionListener1
 
 class ListView : AppCompatActivity() {
 
@@ -11,10 +14,34 @@ class ListView : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view)
 
-        val fruits = arrayOf("Graphes","Pineapples","Apple","Kiwi")
-        val adp:ArrayAdapter<String> = ArrayAdapter(this@ListView,android.R.layout.simple_list_item_1,fruits)
+        val company = arrayOf("apple","tesla","spaceX","samsung","nokia")
+        val adp:ArrayAdapter<String> = ArrayAdapter(this@ListView,android.R.layout.simple_list_item_1,company)
 
         listview.adapter = adp
 
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adp.filter.filter((newText))
+                return true
+            }
+
+        })
+        
+
+
+
+        imageButton.setOnClickListener {
+            val intent = Intent(this@ListView,AaryanMainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
+
+
